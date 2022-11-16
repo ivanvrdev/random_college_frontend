b4715dd025fade254d3026b211c96d7f94847ab4
ivanvrdev <ivangaston20032017@gmail.com>
2022-11-15 22:55:38 -0300

    Obtener y filtrar materia por id 15-11-2022

--- src/pages/Subject/index.jsx
+++ src/pages/Subject/index.jsx
@@ -1,11 +1,24 @@
 import React, { useEffect } from 'react'
 import { connect } from 'react-redux'
+import { useParams } from 'react-router-dom'
 
 import Banner from '../../components/Banner'
 
-const Subject = ({subject}) => {
+import { getSubjectById } from '../../redux/actions/subjects'
+
+const Subject = ({subject, getSubjectById}) => {
+
+  const { subjectId } = useParams()
+
+  useEffect(()=>{
+    if(!subject){
+      getSubjectById(subjectId)
+    }
+  }, [])
+
   return (
-    // <Banner header={subject.name} subHeader={subject.degrees[0].name}/>
+    subject ?
+    <Banner header={subject.name} subHeader={subject.degrees[0].name}/> :
     JSON.stringify(subject)
   )
 }
@@ -16,4 +29,4 @@ const mapStateToProps = state => {
   }
 }
 
-export default connect(mapStateToProps)(Subject)
+export default connect(mapStateToProps, {getSubjectById})(Subject)
--- src/redux/actions/subjects.js
+++ src/redux/actions/subjects.js
@@ -13,3 +13,14 @@ export const getSubjects = (userId, userType = 'student') => async dispatch => {
 export const findSubject = (subjectId) => dispatch => {
   dispatch({type: 'FIND_SUBJECT', payload: subjectId})
 }
+
+export const getSubjectById = (subjectId) => async dispatch => {
+  try {
+    const response = await axios.get(`/backend/subject/${subjectId}`)
+
+    dispatch({type: 'SELECT_SUBJECT', payload: response.data.subject})
+
+  } catch(error) {
+    console.log(error)
+  }
+}
